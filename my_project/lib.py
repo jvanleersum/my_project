082229859fe9ed3ff001d1a651337569c23cc801
jvanleersum <75987870+jvanleersum@users.noreply.github.com>
2021-05-31 14:33:25 +0200

    Initial Push

---
+++ my_project/lib.py
@@ -0,0 +1,6 @@
+### Create try_me function
+
+def try_me(name, city):
+    print(f"Hi {name}, Summer is almost here! Enjoy your time in {city}")
+    weather_condition = "Good"
+    return weather_condition
